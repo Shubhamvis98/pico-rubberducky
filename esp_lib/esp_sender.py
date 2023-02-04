7d6d34dfa8eea8b9e1f087686c4ceb12d7a54b8a
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2023-02-05 00:40:06 +0530

    Update esp_sender.py

--- esp_lib/esp_sender.py
+++ esp_lib/esp_sender.py
@@ -15,6 +15,7 @@ class DUCKY:
     def send_line(self, line):
         self.LED.value(0)
         self.uart.write(str(line) + '\n')
+        sleep_ms(len(str(line))*15)
         self.LED.value(1)
 
     def send_serial(self):
@@ -25,7 +26,6 @@ class DUCKY:
         with open(self.ducky, 'r') as file:
             for i in file.readlines():
                 self.send_line(i)
-                sleep_ms(len(str(i))*15)
         
 
 if __name__ == '__main__':
