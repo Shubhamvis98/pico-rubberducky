07b4b5ff010df5b9b84e5c9cc449fefc5a1a7bc1
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2023-02-08 23:08:49 +0530

    Create myfuncs.py

---
+++ esp_lib/myfuncs.py
@@ -0,0 +1,35 @@
+# AUTHOR: SHUBHAM VISHWAKARMA
+# GITHUB: SHUBHAMVIS98
+
+import network
+import config
+from time import sleep
+from machine import Pin
+
+
+LED = Pin(2, Pin.OUT)
+
+def blink(times):
+    for i in range(times):
+        sleep(0.1)
+        LED.value(0)
+        sleep(0.1)
+        LED.value(1)
+
+def wlan(ssid, passwd):
+    sta = network.WLAN(network.STA_IF)
+    ap = network.WLAN(network.AP_IF)
+    ap.active(False)
+    sta.active(True)
+    sta.connect(ssid, passwd)
+    sleep(5)
+    if sta.isconnected():
+        blink(5)
+        return sta.ifconfig()[0]
+    else:
+        sta.active(False)
+        ap.active(True)
+        ap.config(essid='MicPy', password='MicropythoN')
+        blink(2)
+        return ap.ifconfig()[0]
+
